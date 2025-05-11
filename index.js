cadebab3ffec6bd1d798aaa95e08e03e3689a001
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-05-11 09:42:52 +0100

    finalize on checkWinner function

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ function initializeGame(){
     cells.forEach(cell=> cell.addEventListener("click", cellClicked));
     restartBtn.addEventListener("click", restartGame);
     statusText.textContent = `${currentPlayer}'s turn`;
+    running = true;
 
 }
 function cellClicked(){
@@ -35,13 +36,44 @@ function cellClicked(){
 
 }
 function updateCell(cell,index){
+    options[index] = currentPlayer;
+    cell.textContent = currentPlayer;
 
 }
 function changePlayer(){
+    currentPlayer = (currentPlayer == "X") ? "O" : "X";
+    statusText.textContent = `${currentPlayer}'s turn`
 
 }
 function checkWinner(){
+    let roundWon = false;
 
+    for(let i = 0; i < winConditions.length; i++){
+        const condition = winConditions[i];
+        const cellA = options[condition[0]];
+        const cellB = options[condition[1]];
+        const cellC = options[condition[2]];
+
+        if(cellA == "" || cellB == "" || cellC == ""){
+            continue;
+        }
+        if( cellA == cellB && cellB == cellC){
+            roundWon = true;
+            break;
+        }
+    }
+
+    if (roundWon){
+        statusText.textContent = `${currentPlayer} wins!`;
+        running = false;
+    }
+    else if(!options.includes("")){
+        statusText.textContent = `Draw`;
+        running = false;
+    }
+    else{
+        changePlayer();
+    }
 }
 function restartGame(){
 
